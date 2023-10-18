404ae84f84670ffa6e61ed6a696519ee43eebec8
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2023-10-17 21:52:43 -0400

    finish ala6

--- ALA/ALA6/PrintRequest.java
+++ ALA/ALA6/PrintRequest.java
@@ -1,3 +1,6 @@
+/**
+ * public class PrintRequest that implements Comparable
+ */
 public class PrintRequest implements Comparable < PrintRequest > {
     public enum groups {
         ROOT,
@@ -8,32 +11,68 @@ public class PrintRequest implements Comparable < PrintRequest > {
     private int id;
     private String group;
     private long size;
-
+    /**
+     * printrequest constructor method
+     * @param id
+     * @param gr
+     * @param size
+     */
     public PrintRequest(int id, String gr, long size) {
         this.id = id;
         group = gr;
         this.size = size;
     }
+    /**
+     * id getter method
+     * @return int of id
+     */
     public int getID() {
         return id;
     }
+    /**
+     * getter method for group
+     * @return String of group
+     */
     public String getGroup() {
         return group;
     }
+
+    /**
+     * getter method for size
+     * @return long of the size
+     */
     public long getSize() {
         return size;
     }
 
+    /**
+     * setter method for ID
+     * @param id
+     */
     public void setID(int id) {
         this.id = id;
     }
+
+    /**
+     * setter method for group
+     * @param gr group
+     */
     public void setGroup(String gr) {
         group = gr;
     }
+
+    /**
+     * setter method for size
+     * @param s size
+     */
     public void setSize(long s) {
         size = s;
     }
 
+    /**
+     * toString method
+     * @return String to attributes
+     */
     public String toString() {
         double convertedSize = size;
         String unit = "bytes";
@@ -47,18 +86,37 @@ public class PrintRequest implements Comparable < PrintRequest > {
             convertedSize /= 1000;
             unit = "KB";
         }
-        String[] types = {"root", "admin", "user", "batch"};
+        String[] types = {
+            "root",
+            "admin",
+            "user",
+            "batch"
+        };
         return String.format("%5d\t%-5s\t%5.1f%s", id, group, convertedSize, unit);
     }
 
+    /**
+     * public compareTo method
+     * @return int of compare
+     * @param pr printRequest
+     */
     public int compareTo(PrintRequest pr) {
         return this.getPriority() - pr.getPriority();
     }
 
+    /**
+     * getter method for priority
+     * @return int priority
+     */
     private int getPriority() {
-        String[] priorities = {"root", "admin", "user", "batch"};
-        for(int i=0; i<priorities.length; i++) {
-            if(priorities[i].equals(group)) {
+        String[] priorities = {
+            "root",
+            "admin",
+            "user",
+            "batch"
+        };
+        for (int i = 0; i < priorities.length; i++) {
+            if (priorities[i].equals(group)) {
                 return i;
             }
         }
--- ALA/ALA6/Test.java
+++ ALA/ALA6/Test.java
@@ -5,7 +5,14 @@ import java.util.EmptyStackException;
 import java.util.Scanner;
 import java.util.PriorityQueue;
 
+/**
+ * public class Test
+ */
 public class Test {
+    /**
+     * public method main
+     * @param args
+     */
     public static void main(String[] args) {
         Stack < Integer > postfixStack = new Stack < > ();
         Scanner keyboard = new Scanner(System.in);
@@ -44,6 +51,8 @@ public class Test {
                 int result = postfixStack.pop();
                 if (postfixStack.isEmpty()) {
                     System.out.println("Result = " + result);
+                    System.out.println("Do you want to evaluate another postfix expression? (yes/no):");
+                    expression = keyboard.nextLine();
                 } else {
                     System.out.println("Malformed postfix expression");
                     System.out.println("Do you want to evaluate another postfix expression? (yes/no):");
@@ -74,11 +83,35 @@ public class Test {
             readFile.close();
             //process printing requests
             long speed = 10000; //10,000 bytes/s
+            long totalTime = 0;
+            String time = "0:0:0:0";
+
+            System.out.println("Summary of the printed requests");
+            System.out.println("-------------------------------------------------------------");
+            System.out.println("User ID Group   Size   Completion time");
+
             while (!printingQueue.isEmpty()) {
                 PrintRequest pr = printingQueue.poll();
-                long time = pr.getSize() / speed;
+                long timeSec = pr.getSize() / speed;
+                long sec = timeSec % 60;
+                long minutes = timeSec % 3600 / 60;
+                long hours = timeSec % 86400 / 3600;
+                long days = timeSec / 86400;
+
+                time = days + ":" + hours + ":" + minutes + ":" + sec;
+                totalTime += timeSec;
                 System.out.println(pr + "\t" + time);
             }
+
+            long sec = totalTime % 60;
+            long minutes = totalTime % 3600 / 60;
+            long hours = totalTime % 86400 / 3600;
+            long days = totalTime / 86400;
+
+            time = days + ":" + hours + ":" + minutes + ":" + sec;
+            totalTime += totalTime;
+            System.out.println();
+            System.out.println("Total Printing time: " + "\t" + time);
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
         }
