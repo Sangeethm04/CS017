2d7c291fa2316fc749241e1cfacf24abde6bb156
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2023-10-28 13:30:35 -0400

    ev

--- Split/Test.java
+++ Split/Test.java
@@ -1,28 +1,31 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
         String[] emails = new String[1600];
         File file = new File("emails.txt");
+       
         try {
+            try (PrintWriter prints = new PrintWriter("meo.txt")) {
+                Scanner read = new Scanner(file);
+                    String nextLine = read.nextLine();
+                    emails = nextLine.split("\\,");
 
-            Scanner read = new Scanner(file);
-            while (read.hasNextLine()) {
-                String nextLine = read.nextLine();
-                emails = nextLine.split("\\,");
-
+                for (int i = 0; i < emails.length; i++) {
+                //print to print writer
+                prints.println(emails[i]);
+      }
+                read.close();
             }
-            read.close();
 
         } catch (FileNotFoundException e) {
             System.out.println("File not found.");
             System.exit(0);
         }
 
-        for (int i = 0; i < emails.length; i++) {
-            System.out.println(emails[i]);
-        }
+        
     }
 }
