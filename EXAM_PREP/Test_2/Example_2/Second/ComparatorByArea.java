2909836621b61d926eec047ba37f7d3e27589b76
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2023-10-28 22:16:35 -0400

    ev

--- EXAM_PREP/Test_2/Example_2/Second/ComparatorByArea.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class ComparatorByArea implements Comparator<Country>{
-    public int compare(Country a, Country b) {
-        Double a1 = a.getArea();
-        Double a2 = b.getArea();
-        return a1.compareTo(a2);
-    }
-}
--- EXAM_PREP/Test_2/Example_2/Second/ComparatorByName.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class ComparatorByName implements Comparator<Country>{
-    public int compare(Country a, Country b) {
-        String a1 = a.getName();
-        String a2 = b.getName();
-        return a1.compareTo(a2);
-    }
-}
